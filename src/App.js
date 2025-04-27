5022ea12243e82345fcbb188c4a8fea1698bba1b
SaeedAfshari11235 <msafshari11235@gmail.com>
2025-04-27 13:06:46 +0330

    Add get request handler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+
+  useEffect(() => {
+    setIsPending(true);
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodo(data);
+        setIsPending(false);
+        console.log(data);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
